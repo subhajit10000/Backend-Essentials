fe347d86b50955cefa9e922897e25252872ef400
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-18 09:08:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,3 +49,107 @@ const authMiddleware = require("../middlewares/auth.middleware.js");
 const { profile } = require("../controllers/user.controller.js");
 router.get("/profile", authMiddleware, profile);
 module.exports = router;
+
+
+
+
+
+//auth.middleware.js
+//-------------------
+const { verifyToken } = require("../utils/jwt.util.js");
+const authMiddleware = (req, res, next) => {
+    console.log(req.headers['authorization']);
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "Token missing"
+        })
+    }
+    // bearer sgfifiugruifgriufrugfsufgoiuwrtwr4ry824ry48rweiurgkufgk
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = verifyToken(token); // {token-> user.id , user.email }
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid Token"
+        })
+    }
+}
+module.exports = authMiddleware;
+
+
+
+
+
+
+
+//user.middleware.js
+//------------------
+
+
+
+
+
+
+//auh.controller.js
+//---------------------
+
+const { helloService, registerUser, loginUser } = require('../services/auth.service.js');
+const hello = (req, res) => {
+    const data = helloService();
+    res.send(data);
+}
+const register = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const user = await registerUser(name, email, password);
+
+        res.status(201).json({
+            "success": true,
+            "data": user
+        })
+
+    } catch (error) {
+        next(error);
+    }
+}
+const login = async (req, res, next) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const loggedInUser = await loginUser(email, password);
+
+        res.status(200).json({
+            success: true,
+            loggedInUser
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+module.exports = { hello, register, login }
+
+
+
+
+
+
+//user.controller.js
+//-------------------------
+const { getUserById } = require("../services/user.service.js")
+const profile = async (req, res, next) => {
+    console.log(req.user.id);
+
+    const user = await getUserById(req.user.id);
+    res.json({
+        success: true,
+        data: user
+    })
+}
+module.exports = { profile }
